c1b227f2e205b0792ca39d9b0c8b09de113971c4
NanArmor <32504275+NanArmor@users.noreply.github.com>
2019-01-08 19:09:28 -0600

    Add files via upload

---
+++ PavlovUGCMigrator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pavlov UGC Migrator")]
+[assembly: AssemblyDescription("Tool to move files between UE4 Pavlov UGC folders")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nan")]
+[assembly: AssemblyProduct("PavlovUGCMigrator.Properties")]
+[assembly: AssemblyCopyright("lol")]
+[assembly: AssemblyTrademark("lmao")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b8180bab-114b-45f5-9362-5c79373821a4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
